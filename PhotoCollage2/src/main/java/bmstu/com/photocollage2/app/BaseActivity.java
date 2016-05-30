382fe278707aab02102a885df1df9a6dd7be03f0
Maida Sardarova <maida17@mail.ru>
2016-05-30 15:07:57 +0300

    Update BaseActivity.java

--- PhotoCollage2/src/main/java/bmstu/com/photocollage2/app/BaseActivity.java
+++ PhotoCollage2/src/main/java/bmstu/com/photocollage2/app/BaseActivity.java
@@ -7,20 +7,14 @@ import android.os.Bundle;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
-/**
- * @author Paresh Mayani (@pareshmayani)
- */
 public abstract class BaseActivity extends Activity {
 
     protected ImageLoader imageLoader = ImageLoader.getInstance();
 
-    //   BaseActivity.imageLoader.init(ImageLoaderConfiguration.createDefault(getBaseCont‌​ext()));
-    //  imageLoader.init(ImageLoaderConfiguration.createDefault(this));
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
-
+        
         imageLoader = ImageLoader.getInstance();
         imageLoader.init(ImageLoaderConfiguration.createDefault(this));
     }
